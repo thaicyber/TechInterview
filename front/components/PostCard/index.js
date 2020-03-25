b65699af22b3a29e89d1ddd35e2f3fba21d508a1
palkim <hrb0120@gmail.com>
2020-03-25 14:00:12 +0900

    로직 변경 : main에서 loadMainPosts 요청 후 받은 데이터를 PostCard 컴포넌트에게 props로 전달로 변경

--- front/components/PostCard/index.js
+++ front/components/PostCard/index.js
@@ -2,18 +2,11 @@ import React, { useEffect } from "react";
 import { PostCardWrapper, Content } from "./style";
 import { useDispatch, useSelector } from "react-redux";
 import { LOAD_MAIN_POSTS_REQUEST } from "../../reducers/post";
-const PostCard = () => {
-  const dispatch = useDispatch();
+const PostCard = ({ post }) => {
   const { me } = useSelector(state => state.user);
-  const { mainPosts } = useSelector(state => state.post);
-  useEffect(() => {
-    dispatch({
-      type: LOAD_MAIN_POSTS_REQUEST
-    });
-  }, []);
   return (
     <PostCardWrapper isLogined={me ? true : false}>
-      {mainPosts && mainPosts.map(item => <Content>{item.content}</Content>)}
+      {post && <Content>{post.title}</Content>}
     </PostCardWrapper>
   );
 };
--- front/containers/NoneMemberLayout/index.js
+++ front/containers/NoneMemberLayout/index.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NoneMemberLayoutWrapper } from "./style";
 import Header from "../../components/Header";
 import RightSideBar from "../../components/RightSideBar";
 import Footer from "../../components/Footer";
 import PostCard from "../../components/PostCard";
 import Nav from "../../components/Nav";
+import { LOAD_MAIN_POSTS_REQUEST } from "../../reducers/post";
+import { useDispatch, useSelector } from "react-redux";
 const NoneMemberLayout = () => {
+  const dispatch = useDispatch();
+  const { mainPosts } = useSelector(state => state.post);
+  useEffect(() => {
+    dispatch({
+      type: LOAD_MAIN_POSTS_REQUEST
+    });
+  }, []);
   return (
     <NoneMemberLayoutWrapper>
       <Header />
-      <PostCard />
+      {mainPosts && mainPosts.map(post => <PostCard post={post} />)}
       <RightSideBar />
       <Footer />
       <Nav />
