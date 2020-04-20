0050253a9ffed21d64101725746faa30f95d187c
palkim <hrb0120@gmail.com>
2020-04-21 00:29:40 +0900

    신규 페이지 생성 : 좋아요 누른 포스트들만 볼 수 있는 likePosts page생성

--- front/components/Nav/index.js
+++ front/components/Nav/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { NavWrapper, NavIconWrapper } from "./style";
-
+import Link from "next/link";
 const Nav = () => {
   return (
     <NavWrapper>
-      <NavIconWrapper>홈</NavIconWrapper>
+      <Link href="/">
+        <NavIconWrapper>
+          <a>홈</a>
+        </NavIconWrapper>
+      </Link>
       <NavIconWrapper>검색</NavIconWrapper>
-      <NavIconWrapper>좋아요</NavIconWrapper>
+      <Link href="/likePosts">
+        <NavIconWrapper>
+          <a>좋아요</a>
+        </NavIconWrapper>
+      </Link>
       <NavIconWrapper>북마크</NavIconWrapper>
     </NavWrapper>
   );
---
+++ front/pages/likePosts.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const likePosts = () => {
+  return <div>likePosts</div>;
+};
+
+export default likePosts;
