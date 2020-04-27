dc3867135ef4803fa33f0508cf1f4092dd42e47f
palkim <hrb0120@gmail.com>
2020-04-27 16:13:59 +0900

    기능 추가: 메인에서 키워드 입력 후 검색하면 hashtag router로 이동하도록 구현 완료

--- front/components/Header/SearchBarForm/index.js
+++ front/components/Header/SearchBarForm/index.js
@@ -1,28 +1,39 @@
 import React, { useState } from "react";
 import { AtdForm, AtdInput } from "./style";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import UserSetting from "../../UserSetting";
 import Avatar from "../../Util/Avatar";
-const onSubmitForm = e => {
-  e.preventDefault();
-  console.log("submit");
-};
+import Router from "next/router";
 const SearchBarForm = () => {
+  const dispatch = useDispatch();
   const [avatarClick, setAvatarClick] = useState(false);
   const { me } = useSelector(state => state.user);
+  const [hashtag, setHashtag] = useState("");
+  const onSubmitForm = e => {
+    e.preventDefault();
+    Router.push(
+      { pathname: "/hashtag", query: { tag: hashtag } },
+      `/hashtag/${hashtag}`
+    );
+    console.log(hashtag);
+    console.log("submit");
+  };
   const onClickAvatar = () => {
     setAvatarClick(true);
     document.body.style.overflowY = "hidden";
+  };
+  const onChangeInput = e => {
+    setHashtag(e.target.value);
   };
   return (
     <AtdForm onSubmit={onSubmitForm}>
       {me ? (
         <>
           <Avatar onClick={onClickAvatar} img={me.img} size="small"></Avatar>
-          <AtdInput htmlType="submit" isLogin={true} />
+          <AtdInput onChange={onChangeInput} htmlType="submit" isLogin={true} />
         </>
       ) : (
-        <AtdInput htmlType="submit" />
+        <AtdInput htmlType="submit" onChange={onChangeInput} />
       )}
       {avatarClick ? (
         <UserSetting
--- front/pages/hashtag.js
+++ front/pages/hashtag.js
@@ -2,15 +2,19 @@ import React from "react";
 import { useSelector } from "react-redux";
 import PostCard from "../components/PostCard";
 import { LOAD_HASHTAG_POSTS_REQUEST } from "../reducers/post";
-const Hashtag = () => {
+import Announce from "../components/Announce";
+const Hashtag = ({ tag }) => {
   const { hashtagPosts } = useSelector(state => state.post);
   // console.log("hashtagPosts", hashtagPosts);
   return (
     <>
-      {hashtagPosts &&
+      {hashtagPosts && hashtagPosts.length > 0 ? (
         hashtagPosts.map(post => (
           <PostCard showMenu={true} post={post} key={post.id} route="hashtag" />
-        ))}
+        ))
+      ) : (
+        <Announce message={`#${tag} 검색된 결과가 없습니다.`} />
+      )}
     </>
   );
 };
