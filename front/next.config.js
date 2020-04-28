da170b57d0b9422e6cbaf7c5a793c6db8ed777c5
palkim <hrb0120@gmail.com>
2020-04-28 13:31:47 +0900

    gzip 사용을 위한 compression-webpack 설치 및 설정 적용

--- front/next.config.js
+++ front/next.config.js
@@ -1,5 +1,6 @@
 const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
 const webpack = require("webpack");
+const CompressionPlugin = require("compression-webpack-plugin");
 module.exports = withBundleAnalyzer({
   distDir: ".next",
   analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
@@ -15,12 +16,15 @@ module.exports = withBundleAnalyzer({
     }
   },
   webpack(config) {
-    console.log("config", config);
     const prod = process.env.NODE_ENV === "production";
     const plugins = [
       ...config.plugins,
       new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/)
     ];
+    if (prod) {
+      // 배포 환경일 때 플러그인 추가해주기
+      plugins.push(new CompressionPlugin()); // index.js.gz
+    }
     return {
       ...config,
       mode: prod ? "production" : "development",
