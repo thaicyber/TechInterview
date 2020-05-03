3bdd160fb2392c5bf4726a5196286d392d8c8055
palkim <hrb0120@gmail.com>
2020-05-04 02:57:18 +0900

    설정 : baseUrl 변경

--- front/config/config.js
+++ front/config/config.js
@@ -1,6 +1,6 @@
 const serverURL =
   process.env.NODE_ENV === "production"
-    ? "https://api.techinterview.kr"
+    ? "http://api.techinterview.kr"
     : "http://localhost:3065";
 
 export { serverURL };
