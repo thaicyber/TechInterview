63a6a9439aa2272ae8c5cede2e899bdda2fd79f6
palkim <hrb0120@gmail.com>
2020-05-03 23:32:00 +0900

    설정 : server.js port 수정

--- front/server.js
+++ front/server.js
@@ -62,7 +62,7 @@ app.prepare().then(() => {
     return handle(req, res);
   });
 
-  server.listen(3060, () => {
-    console.log("next+express running on port http://localhost:3060");
+  server.listen(prod ? process.env.PORT : 3060, () => {
+    console.log(`next+express running on port ${process.env.PORT}`);
   });
 });
